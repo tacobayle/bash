1224f10eaee66b4459fd49a042722cd5e4ad9804
Nicolas <nicolas.bayle.it@gmail.com>
2019-01-25 09:15:00 +0000

    20190125: first commit

---
+++ installDocker.sh
@@ -0,0 +1,7 @@
+apt-get update && sudo apt-get -y upgrade
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce
