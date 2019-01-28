c4d90131eee31fa32b743e17085bf5eedf1e213d
Nicolas <nicolas.bayle.it@gmail.com>
2019-01-28 17:45:04 +0000

    0190128: added k8s

---
+++ installK8s.sh
@@ -0,0 +1,11 @@
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+sudo apt-get update
+
+sudo apt-get install -y kubelet=1.12.2-00 kubeadm=1.12.2-00 kubectl=1.12.2-00
+
+sudo apt-mark hold kubelet kubeadm kubectl
