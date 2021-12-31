941d5b29774a026fa9ec67d743fb60cb9e9cd799
tacobayle <nicolas.bayle@gmail.com>
2021-12-31 16:06:24 +0000

    20211231

--- vcenter/get_vcenter.sh
+++ vcenter/get_vcenter.sh
@@ -10,19 +10,19 @@ if [[ ${#token} -ne 32 ]] ; then
 fi
 if [[ $4 == "datacenters" ]] ; then
   url="rest/vcenter/datacenter"
-  raw_dcs=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datacenter))')
+  raw_dcs=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datacenter)) | map([.name]) | add')
   echo $raw_dcs
 elif [[ $4 == "clusters" ]] ; then
   url="rest/vcenter/cluster"
-  raw_clusters=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.drs_enabled)) | map(del(.cluster)) | map(del(.ha_enabled))')
+  raw_clusters=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.drs_enabled)) | map(del(.cluster)) | map(del(.ha_enabled)) | map([.name]) | add')
   echo $raw_clusters
 elif [[ $4 == "datastores" ]] ; then
   url="rest/vcenter/datastore"
-  raw_clusters=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datastore)) | map(del(.free_space)) | map(del(.capacity)) | map(del(.type))')
+  raw_clusters=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datastore)) | map(del(.free_space)) | map(del(.capacity)) | map(del(.type)) | map([.name]) | add')
   echo $raw_clusters
 elif [[ $4 == "networks" ]] ; then
   url="rest/vcenter/network"
-  raw_networks=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.network)) | map(del(.type))')
+  raw_networks=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.network)) | map(del(.type)) | map([.name]) | add')
   echo $raw_networks
 else
   echo 'Illegal parameter: $4 should be equal to one of the following: datacenters, clusters, datastores, networks'
