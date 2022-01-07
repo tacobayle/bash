55077b3906889a7dffd42540b525b479735a516b
tacobayle <nicolas.bayle@gmail.com>
2022-01-07 09:29:04 +0000

    20220107_vcenter

--- vcenter/get_vcenter.sh
+++ vcenter/get_vcenter.sh
@@ -3,6 +3,10 @@ if [ "$#" -ne 3 ]; then
     echo "Illegal number of parameters"
     exit 255
 fi
+rm -f datacenters.json
+rm -f clusters.json
+rm -f datastores.json
+rm -f networks.json
 token=$(curl -k -s -X POST -u $2:$3 https://$1/rest/com/vmware/cis/session -H "Content-Type: application/json" | jq .value | tr -d \")
 if [[ ${#token} -ne 32 ]] ; then
   echo "Token does not look right"
@@ -10,13 +14,13 @@ if [[ ${#token} -ne 32 ]] ; then
 fi
 url="rest/vcenter/datacenter"
 raw_dcs=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datacenter)) | map([.name]) | add')
-echo $raw_dcs | tee datacenters.json
+echo $raw_dcs | tee datacenters.json >/dev/null
 url="rest/vcenter/cluster"
 raw_clusters=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.drs_enabled)) | map(del(.cluster)) | map(del(.ha_enabled)) | map([.name]) | add')
-echo $raw_clusters | tee clusters.json
+echo $raw_clusters | tee clusters.json >/dev/null
 url="rest/vcenter/datastore"
 raw_datastore=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.datastore)) | map(del(.free_space)) | map(del(.capacity)) | map(del(.type)) | map([.name]) | add')
-echo $raw_datastore | tee datastores.json
+echo $raw_datastore | tee datastores.json >/dev/null
 url="rest/vcenter/network"
 raw_networks=$(curl -k -s -X GET "https://$1/${url}" -H "vmware-api-session-id:${token}" -H "Content-Type: application/json" | jq '.value | map(del(.network)) | map(del(.type)) | map([.name]) | add')
-echo $raw_networks | tee networks.json
+echo $raw_networks | tee networks.json >/dev/null
