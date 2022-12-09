c8dc1ad8c19fa8c02f67cbcca1de4fa27be405c8
tacobayle <nicolas.bayle@gmail.com>
2022-12-09 10:12:08 +0000

    20221209_api_session_vsphere8

--- sed/remove_anything_after_a_patern.sh
+++ sed/remove_anything_after_a_patern.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-basename "https://portal.avipulse.vmware.com/api/portal/download/SoftwaresDownloads/Version-21.1.4/controller-21.1.4-9210.ova?X-Amz-Algorithm=AWS4-HMAC-SHA" | sed -e 's/.ova.*/.ova/g'
+basename 'https://portal.avipulse.vmware.com/api/portal/download/SoftwaresDownloads/Version-21.1.4/controller-21.1.4-9210.ova?X-Amz-Algorithm=AWS4-HMAC-SHA' | sed -e 's/.ova.*/.ova/g'
