1c5d04d2c1107e8689ceafbacb383e31107e5650
tacobayle <nicolas.bayle@gmail.com>
2022-12-09 10:24:05 +0000

    20221209_api_session_vsphere

--- vcenter/create_vcenter_api_session.sh
+++ vcenter/create_vcenter_api_session.sh
@@ -5,12 +5,14 @@
 # $3 is the password
 # $4 is the vCenter FQDN
 #
+# Tested with vSphere7 and vSphere 8
+#
 retry=6
 pause=10
 attempt=0
 while true ; do
   if [[ $2 == '' ]]; then
-    response=$(curl -k -s --write-out "\n%{http_code}" -X POST -u "$1:$3" https://$4/rest/com/vmware/cis/session -H "Content-Type: application/json")
+    response=$(curl -k -s --write-out "\n%{http_code}" -X POST -u "$1:$3" https://$4/api/session -H "Content-Type: application/json")
   else
     response=$(curl -k -s --write-out "\n%{http_code}" -X POST -u "$1@$2:$3" https://$4/api/session -H "Content-Type: application/json")
   fi
