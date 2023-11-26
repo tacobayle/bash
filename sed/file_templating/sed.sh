9e143ce7949d023cb9a58c534fb4037f62fc0cd1
tacobayle <nicolas.nayle@gmail.com>
2023-11-26 16:32:00 +0200

    20232611_sed

--- sed/file_templating/sed.sh
+++ sed/file_templating/sed.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 i=1
 word="dog"
-sed -e "s/\${i}/${i}/" -e "s/\${word}/${word}/" template.txt.template | tee template.txt > /dev/null
+sed -e "s/\${i}/${i}/" \
+    -e "s/\${word}/${word}/" \
+    -e "s/\${date}/$(date)/" \
+    template.txt.template | tee template.txt > /dev/null
