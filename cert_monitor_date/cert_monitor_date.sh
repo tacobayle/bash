facab96555bfa49b48ccf5e449ec9172407ade6a
tacobayle <nicolas.nayle@gmail.com>
2025-03-19 12:38:44 +0100

    20250319_cert_monitor

--- cert_monitor_date/cert_monitor_date.sh
+++ cert_monitor_date/cert_monitor_date.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 #
 fqdn=$1
+webhook_url=$2
 timeout=3
 threshold=10
+#
+# google chat function
+#
+send_google_chat_message() {
+  local url="$1"
+  local message="$2"
+
+  if [[ -z "$url" || -z "$message" ]]; then
+    echo "Error: Webhook URL or message is missing."
+    return 1
+  fi
+
+  local payload='{"text":"'$message'"}'
+
+  curl -X POST -H "Content-Type: application/json; charset=UTF-8" -d "$payload" "$url"
+}
+#
+# cert check
 #
 cert_info=$(timeout $timeout openssl s_client -connect $fqdn:443 2>/dev/null | openssl x509 -noout -dates)
 #
 expiration_date=$(echo "$cert_info" | grep 'notAfter' | cut -d= -f2)
 expiration_date_readable=$(date -d "$expiration_date" +'%Y-%m-%d')
+today_date=$(date +'%Y-%m-%d')
 days_remaining=$((($(date -d "$expiration_date" +%s) - $(date +%s)) / 86400))
-if [[ $days_remaining -le $threshold ]]; then
-  echo "WARNING: Certificate for $fqdn expires on $expiration_date_readable ($days_remaining days remaining)"
-  # You can add actions here, like sending notifications, logging, etc.
-else
-  echo "Certificate for $fqdn is valid until $expiration_date_readable ($days_remaining days remaining)"
+if [[ ${expiration_date_readable} < ${today_date} ]]; then
+  message="CRITICAL: ${fqdn}, Certificate is expired since ${expiration_date_readable}"
 fi
+if [[ $days_remaining -le $threshold ]]; then
+  message="WARNING: ${fqdn}, Certificate for $fqdn expires on $expiration_date_readable ($days_remaining days remaining)"
+else
+  message="INFO: ${fqdn}, Certificate for $fqdn is valid until $expiration_date_readable ($days_remaining days remaining)"
+fi
+send_google_chat_message "$webhook_url" "$message"
